47498a33906436124ee8f8207f58dc6782741dd6
Adiv Paradise <paradise@astro.utoronto.ca>
2018-12-31 13:33:21 -0500

    Updated to match new gradle guidelines and deprecations

---
+++ app/build/generated/source/buildConfig/lite/debug/se/chai/cardboardremotedesktop/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package se.chai.cardboardremotedesktop;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "se.chai.cardboardremotedesktop.lite";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "lite";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "1.3";
+}
